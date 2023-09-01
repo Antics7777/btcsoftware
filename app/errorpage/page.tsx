bb857fb2e4566133caedd6d40be460c317a34cb7
DanielDerefaka <bryancagedx@gmail.com>
2023-09-01 14:47:04 +0100

    done

--- app/errorpage/page.tsx
+++ app/errorpage/page.tsx
@@ -9,9 +9,9 @@ const page = () => {
 
                 <Image src={qr} alt='qr' width={300} height={300} />
             </div>
+            <p className='text-[23px] text-white text-center justify-center'>OOPS Something went wrong</p>
             <div className='text-center justify-center flex  lg:ml-[200px] ml-[30px]'>
 
-            <p>OOPS Something went wrong</p>
             
             <input type="text" className=' bg-transparent placeholder:text-white text-white h-10 w-full  focus:outline-none rounded-md pl-3  border-gray-1' placeholder='You need 2000 USDT to Licence this transaction ' />
             </div>
